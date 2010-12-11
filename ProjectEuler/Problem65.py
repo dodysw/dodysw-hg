e7b2d77aa6f76dcf717cda8ea6d5babbaeaed2dd
dodysw <dodysw@gmail.com>
2010-12-11 12:31:03 -0600

    some project euler solutions

--- ProjectEuler/Problem65.py
+++ ProjectEuler/Problem65.py
@@ -1,4 +1,12 @@
-def convergent(n):
+"""
+2/1, 3/1, 8/3, 11/4, 19/7, 87/32, 106/39, 193/71
+n2 = n1*d0+n0
+106 = 87*7+19
+
+"""
+
+#http://en.wikipedia.org/wiki/Euler%27s_continued_fraction_formula
+def e_convergent(n):
     print "sqrt(%d) = %s" % (n,n**0.5)
     num = int(n**0.5)+n
     den = n*int(n**0.5)
--- ProjectEuler/Problem80.py
+++ ProjectEuler/Problem80.py
@@ -1,11 +1,19 @@
 from time import time
 st = time()
 
+limit = 100
+
+#http://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+def sqrt():
+    
 
 def try1():
     total_digit_sum = 0
     for n in xrange(101):
-        if n**0.5 % 1 == 0:
+        if n**0.5 % 1 != 0:
+            #continued fraction rule
+            a = int(n**0.5)
+            
             
             total_digit_sum +=
             
